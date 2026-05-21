75ee06d256759489d12b5eb584ccd79a2d60ca4a
Alt <ubusievnaran@gmail.com>
2026-05-21 18:35:00 +0300

    init project

---
+++ TTS/Models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace TTS.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Login { get; set; } = string.Empty;
+        public string HashedPassword { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ TTS/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TTS.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
