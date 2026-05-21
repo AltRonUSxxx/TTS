0f2e14130500e86e6a09f8f2ccd7e30d7dd45129
Alt <ubusievnaran@gmail.com>
2026-05-21 18:43:39 +0300

    Add AppDbContext

---
+++ TTS/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TTS.Models;
+
+namespace TTS.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<SpeechLog> SpeechLogs { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
