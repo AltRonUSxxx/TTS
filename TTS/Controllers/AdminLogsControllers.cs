c1f412072602f78c335d44d2b2a2649a09674f03
Alt <ubusievnaran@gmail.com>
2026-05-24 10:10:19 +0300

    Add AdmingLogsController

---
+++ TTS/Controllers/AdminLogsControllers.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TTS.Services;
+
+namespace TTS.Controllers
+{
+    public class AdminLogsControllers : Controller
+    {
+        private readonly ISpeechLogService _speechLogService;
+        private readonly ICurrentUserSevice _currentUserSevice;
+
+        public AdminLogsControllers(ISpeechLogService speechLogService, ICurrentUserSevice currentUserSevice)
+        {
+            _speechLogService = speechLogService;
+            _currentUserSevice = currentUserSevice;
+        }
+
+        public IActionResult Index()
+        {
+            if(!_currentUserSevice.IsLoggedIn(HttpContext))
+            {
+                return RedirectToPage("/index");
+            }
+            var log = _speechLogService.GetAllLogs();
+            return View(log);
+        }
+    }
+}
